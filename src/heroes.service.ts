d4d8e06a040be1d419b9ad81c45e093a987af309
Vivek Menon <vivekpmenon@gmail.com>
2016-03-24 12:04:42 -0400

    Create heroes.service.ts

---
+++ src/heroes.service.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+class HeroesService{
+    
+    constructor(){
+        
+    }
+    
+    getHeroes():string[] {        
+        return ['Super Man', 'Spidey', 'Iron Man'];
+    }
+}
+
+export default HeroesService;
